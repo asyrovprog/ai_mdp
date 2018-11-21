8423b37f2e2a4cf26c7a36fef9672e6c0e9ec1fc
asyrov <asyrovprog@gmail.com>
2018-11-20 16:46:32 -0800

    more changes

--- linar.py
+++ linar.py
@@ -1,8 +1,10 @@
-def reduce_row_down(A, i):
+def reduce_row_down(A, i, L = None):
     if A[i][i] == 0:
         return A
     for o in range(i + 1, len(A)):
         f = A[o][i] / A[i][i]
+        if L is not None:
+            L[o][i] = f
         for j in range(i, len(A[0])):
             A[o][j] = A[o][j] - A[i][j] * f
     return A
@@ -38,16 +40,22 @@ def gaussian_elimination(A):
     normalize_diagonal(A)
     return A
 
-A = [[3, -7, -2, 1, 0, 0],
-     [-3, 5, 1, 0, 1, 0],
-     [6, -4, 0, 0, 0, 1]]
+def LU_decomposition(A):
+    m = len(A)
+    L = [[0 for i in range(m)] for j in range(m)]
+    for i in range(m):
+        L[i][i] = 1
+    for i in range(0, len(A)):
+        reduce_row_down(A, i, L)
+    U = A
+    return L, U
 
-R = gaussian_elimination(A)
-print(A)
 
-[[0.6666666666666666, 1.3333333333333333, 0.5],
- [1.0, 2.0, 0.5],
- [-3.0, -5.0, -1.0]]
+A = [[1, 0, 0, 1, 0, 0], [0, 1, 0, 0, 1, 0], [0, 3, 1, 0, 0, 1]]
+A_ = gaussian_elimination(A)
 
-B
-reduced_row_eshelon_form
+[[1.0, 0.0, 0.0],
+ [0.0, 1.0, 0.0],
+ [0.0,-3.0, 1.0]]
+
+print(A_)
